55f73f3e8c648d09167d779bf35d0b58135e0f94
kevin lee <kevin3Lee@outlook.com>
2018-10-01 10:59:52 +0800

    add smt

--- example_6.12/example_6.12/Program.cs
+++ example_6.12/example_6.12/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace example_6._12
 {
+    class RandomNumberClass
+    {
+        private static Random RandomKey;
+
+        static RandomNumberClass()
+        {
+            RandomKey = new Random(); 
+        }
+
+        public int GetRandomNumber()
+        {
+            return RandomKey.Next();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            RandomNumberClass a = new RandomNumberClass();
+            RandomNumberClass b = new RandomNumberClass();
+
+            Console.WriteLine("Next Random #: {0}", a.GetRandomNumber());
+            Console.WriteLine("Next Random #: {0}", b.GetRandomNumber());
         }
     }
 }
