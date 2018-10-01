f897795ebec7c786e81d4e0f03940510b93da546
kevin lee <kevin3Lee@outlook.com>
2018-10-01 22:50:30 +0800

    private set get

---
+++ example_6.18/example_6.18/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example_6._18
+{
+    class Person
+    {
+        public string Name { get; private set; }
+        //
+        public Person(string name)
+        {
+            Name = name; // set
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Capt. Ernest Evans");
+            Console.WriteLine("Person's name is {0}", p.Name); // get
+        }
+    }
+}
