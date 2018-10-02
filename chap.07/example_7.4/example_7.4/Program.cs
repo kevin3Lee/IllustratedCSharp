b36b943ba5214ced1a8251608490f35c6c749201
kevin lee <kevin3Lee@outlook.com>
2018-10-02 11:44:24 +0800

    null

---
+++ chap.07/example_7.4/example_7.4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example_7._4
+{
+    class SomeClass
+    {
+        public string Field1 = "SomeClass Field1";
+        public void Method1(string value)
+        {
+            Console.WriteLine("SomeClass.Method1: {0}", value);
+        }
+    }
+    //
+    class OtherClass : SomeClass
+    {
+        new public string Field1 = "OtherClass Field1";
+
+        new public void Method1(string value)
+        {
+            Console.WriteLine("OtherClass.Method1: {0}", value);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OtherClass oc = new OtherClass();
+            oc.Method1(oc.Field1);
+        }
+    }
+}
