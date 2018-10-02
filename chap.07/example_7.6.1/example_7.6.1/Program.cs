5ca48f694c89ed853828748b1838742835694521
kevin lee <kevin3Lee@outlook.com>
2018-10-02 14:13:59 +0800

    virtual method and override

---
+++ chap.07/example_7.6.1/example_7.6.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example_7._6._1
+{
+    class MyBaseClass
+    {
+        virtual public void Print()
+        {
+            Console.WriteLine("This if the base class");
+        }
+    }
+
+    class MyDerivedClass : MyBaseClass
+    {
+        override public void Print()
+        {
+            Console.WriteLine("This is the derived class");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDerivedClass derived = new MyDerivedClass();
+            MyBaseClass mybc = new MyBaseClass();
+            MyBaseClass mybc2 = (MyBaseClass) derived;
+
+            derived.Print();
+            mybc.Print();
+            mybc2.Print();
+        }
+    }
+}
