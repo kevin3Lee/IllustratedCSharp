551873d086fcf49d16856cf46c127bb650e15f87
kevin lee <kevin3Lee@outlook.com>
2018-10-02 13:52:13 +0800

    refer base class

---
+++ chap.07/example_7.6.0/example_7.6.0/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example_7._6._0
+{
+    class MyBaseClass
+    {
+        public void print()
+        {
+            Console.WriteLine("This is the base class.");
+        }
+    }
+    
+    class MyDerivedClass : MyBaseClass
+    {
+        new public void Print()
+        {
+            Console.WriteLine("This is the derived class.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDerivedClass derived = new MyDerivedClass();
+            MyBaseClass mybc = (MyBaseClass) derived; // turn into base class
+
+            derived.Print();
+            mybc.print();
+        }
+    }
+}
