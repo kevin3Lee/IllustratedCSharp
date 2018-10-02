76ffd393cdec082ecfc80361de21c16add1ffabb
kevin lee <kevin3Lee@outlook.com>
2018-10-02 12:03:11 +0800

    access the base class

---
+++ chap.07/example_7.5/example_7.5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example_7._5
+{
+    class SomeClass
+    {
+        public string Field1 = "Field1 -- In the base class";
+
+    }
+    //
+    class OtherClass : SomeClass
+    {
+        new public string Field1 = "Field1 -- In the derived class";
+
+        public void PrintField()
+        {
+            Console.WriteLine(Field1); // derived class
+            Console.WriteLine(base.Field1); // base class
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OtherClass oc = new OtherClass();
+            oc.PrintField();
+        }
+    }
+}
