b1622e532bc7a789fef97e53936533c7b3aeb9c4
kevin lee <kevin3Lee@outlook.com>
2018-10-04 16:58:44 +0800

    enum

---
+++ chap.11/example_11.1/example_11.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example_11._1
+{
+    enum TrafficLight
+    {
+        Green,
+        Yellow,
+        Red
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TrafficLight t1 = TrafficLight.Red;
+            TrafficLight t2 = TrafficLight.Green;
+            TrafficLight t3 = t2;
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+        }
+    }
+}
