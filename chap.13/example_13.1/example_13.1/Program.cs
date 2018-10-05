7de29ae2510b83e2ad8a2d2aac8e85ecac5fa189
kevin lee <kevin3Lee@outlook.com>
2018-10-05 17:40:51 +0800

    fix the delegate name and comments

--- chap.13/example_13.1/example_13.1/Program.cs
+++ chap.13/example_13.1/example_13.1/Program.cs
@@ -29,17 +29,17 @@ namespace example_13._1
         static void Main(string[] args)
         {
             MyClass mc = new MyClass(); // ?
-            MyDel del; //decalre delegate **variable**.
+            MyDel del_obj; //decalre delegate **variable**.
             //
             Random rand = new Random();
             int randomValue = rand.Next(99);
             
             // set a del obj include methods
-            del = randomValue < 50 // create a delegate **object**.
-                ? new MyDel(mc.PrintLow)
-                : new MyDel(mc.PrintHigh);
+            del_obj = randomValue < 50 // create a delegate **object**.
+                ? new MyDel(mc.PrintLow) // Create delegate and save ref. Instance method.
+                : new MyDel(mc.PrintHigh); // Create delegate and save ref. Instance method.
 
-            del(randomValue); // execute the delegate object.
+            del_obj(randomValue); // execute the delegate object.
 
         }
     }
