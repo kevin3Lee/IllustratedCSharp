133c037779d312baa2d20b6add11e9c8f2ce9b9f
kevin lee <kevin3Lee@outlook.com>
2018-10-05 12:21:34 +0800

    access to the array

---
+++ chap.12/example_12.6/example_12.6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example_12._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myIntArray;
+            myIntArray = new int[4]; // a new array with 4 ints
+
+            for (int i=0; i < 4; i++)
+                myIntArray[i] = i * 10;
+
+            for(int i=0; i < 4; i++)
+                Console.WriteLine("Value of element {0} = {1}", i, myIntArray[i]);
+        }
+    }
+}
