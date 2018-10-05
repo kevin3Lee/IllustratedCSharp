f90ca0123c9bc60feef6969ffdc74fe9f19aaaa1
kevin lee <kevin3Lee@outlook.com>
2018-10-05 13:23:46 +0800

    foreach()

---
+++ chap.12/example_12.10.0/example_12.10.0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example_12._10._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = {10, 11, 12, 13};
+
+            foreach (int item in arr1)
+                Console.WriteLine("Item Value: {0}", item);
+        }
+    }
+}
