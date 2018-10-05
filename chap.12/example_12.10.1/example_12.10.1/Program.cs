131212100995fc710e113a61286968a72783395c
kevin lee <kevin3Lee@outlook.com>
2018-10-05 16:22:34 +0800

    foreach(int item in array), the item is readonly

---
+++ chap.12/example_12.10.1/example_12.10.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example_12._10._1
+{
+    class MyClass
+    {
+        public int MyField = 0;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass[] mcArray = new MyClass[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                mcArray[i] = new MyClass();
+                mcArray[i].MyField = i;
+            }
+
+            foreach (MyClass item in mcArray)
+            {
+                item.MyField += 10;
+            }
+
+            foreach (MyClass item in mcArray)
+            {
+                Console.WriteLine("{0}",item.MyField);
+            }
+        }
+    }
+}
