bfd5a473fcac2d947d2478153ae3db6af4ceb8c4
kevin lee <kevin3Lee@outlook.com>
2018-10-06 17:00:41 +0800

    using an Iterator to create an Enumerator

---
+++ chap.18/example_18.4.2/example_18.4.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example_18._4._2
+{
+    class MyClass
+    {
+        public IEnumerator<string> GetEnumerator()
+        {
+            return BlackAndWhite(); // return enumerator
+        }
+
+        public IEnumerator<string> BlackAndWhite() // create a Iterator, each "yield return" line is a shade item.
+        {
+            yield return "black"; // yield is the keyword
+            yield return "gray";
+            yield return "white";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+
+            foreach (string shade in mc)
+            {
+                Console.WriteLine(shade);
+            }
+        }
+    }
+}
