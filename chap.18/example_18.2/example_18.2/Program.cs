22974332a823458acba693b579972ca577d024d6
kevin lee <kevin3Lee@outlook.com>
2018-10-06 16:03:56 +0800

    a manually foreach implement

---
+++ chap.18/example_18.2/example_18.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example_18._2
+{
+    class Program
+    {
+        static void Main(string[] args) // does manually foreach statement with a enumerator
+        {
+            int[] MyArray = {10, 11, 12, 13};
+
+            IEnumerator ie = MyArray.GetEnumerator(); // the key method: GetEnumerator()
+
+            while (ie.MoveNext())
+            {
+                int i = (int) ie.Current;
+                Console.WriteLine("{0}",i);
+            }
+        }
+    }
+}
