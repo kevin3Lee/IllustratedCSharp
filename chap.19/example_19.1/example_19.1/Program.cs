e8267c4b99cd5d75a4450dbba8b15111ed64b1d5
kevin lee <kevin3Lee@outlook.com>
2018-10-06 17:28:32 +0800

    A LINQ example

---
+++ chap.19/example_19.1/example_19.1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example_19._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {2, 12, 5, 15};
+
+            IEnumerable<int> lowNums = // define a enumrator to store the query
+                from n in numbers
+                where n < 10
+                select n;
+
+            foreach (var x in lowNums)
+            {
+                Console.Write("{0}, ",x);
+            }
+        }
+    }
+}
